cf24bf0a616de051b90c57dfb7f761794ec20b55
Prateek <prateek.verma0701@gmail.com>
2020-03-30 16:05:42 +0530

    Bean Life cycle - init-method and destroy-method implementation

---
+++ src/com/prateek/springdemo/BeanLifeCycleDemoApp.java
@@ -0,0 +1,24 @@
+package com.prateek.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanLifeCycleDemoApp {
+
+	public static void main(String[] args) {
+
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"beanLifeCycle-applicationContext.xml");
+
+		// retrieve bean from spring container
+		Coach coach = context.getBean("myCoach", Coach.class);
+
+		System.out.println(coach.getDailyWorkOut());
+
+		System.out.println(coach.getDailyFortune());
+
+		// close the context
+		context.close();
+	}
+
+}
--- src/com/prateek/springdemo/TrackCoach.java
+++ src/com/prateek/springdemo/TrackCoach.java
@@ -28,4 +28,15 @@ public class TrackCoach implements Coach {
 
 		return "Just Do IT : " + fortuneService.getFortune();
 	}
+
+	// add an init method
+
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: inside method doMyStartupstuff");
+	}
+
+	// add a destroy method
+	public void doMyCleanupStuff() {
+		System.out.println("TrackCoach: inside method doMyCleanupStuff");
+	}
 }
